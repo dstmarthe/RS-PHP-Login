9327eafbfac16caadc2ecada34ab444979b23005
Dale <72896959+dstmarthe@users.noreply.github.com>
2021-04-19 02:27:25 -0400

    add logiin  and change register code

--- index.php
+++ index.php
@@ -8,9 +8,9 @@ if ( isset($_POST['logout']) ) {
 
 if ( isset($_POST['user']) && isset($_POST['pass']))//This writes user registration into a json file
 { 
-        $file = fopen(“register.js”,'a');
-        $account = '{"Username":'.htmlentities($_POST['user']).
-            ',"Password":'.htmlentities($_POST['pass']);
+        $file = fopen("register.php",'a');
+        $account = htmlentities($_POST['user'])." /n".
+            htmlentities($_POST['pass']);
         fwrite($file, $account);
         fclose($file);
 }
@@ -18,16 +18,16 @@ if ( isset($_POST['user']) && isset($_POST['pass']))//This writes user registrat
 ?>
 <html>
 <head><title>Dale Stmarthe</title></head><body>
-<h1><strong>Register</strong></h1>
+<a href="login.php">Please log in</a>
+echo<h1><strong>Register</strong></h1>
 <form method="post">
 <p>Username:
 <input type="text" name="user" size="40"></p>
 <p>Password:
 <input type="text" name="pass"></p>
 <p><input type="submit" value="Sign Up"/></p>
-<form method="POST">
+
 <p><button name="logout">logout</button></p>
-</form>
 </form>
 <?php
 
